b2fdb14bc10b30655bd542207da3cfcd88b68122
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-01-30 14:56:00 -0500

    footer partial completion

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -1,11 +1,11 @@
 class Furniture < ApplicationRecord
   belongs_to :user
   has_many :bundled_furnitures
-  has_one :photo
+  has_one_attached :photo
   has_many :bundles_offer, through: :bundled_furnitures
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true
-  validates :photo, presence: true
+  # validates :photos, presence: true
   # validates :dimensions, presence: true
   # validates :condition, presence: true, inclusion: { in: %w(new good used), message: "%{value} is not a valid size" }
 end
