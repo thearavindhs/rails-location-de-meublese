7465409d2606f4f83bc7c029bbeabc8202023d99
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-02-24 17:47:21 -0500

    facebook oauth complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_045422) do
+ActiveRecord::Schema.define(version: 2021_02_24_160801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,11 @@ ActiveRecord::Schema.define(version: 2021_01_29_045422) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
